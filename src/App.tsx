42e59c4fb7c4743d37c5e1553d5155bebf8411e7
Minato512 <akagishotaro44@gmail.com>
2022-02-15 18:36:00 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Counter } from "components/Counter/Counter";
+
+function App() {
+  return (
+    <div>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
